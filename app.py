df05df87013f946991b38790d688a61416e57268
joshrdlr <joshuarosasib@gmail.com>
2024-09-21 23:39:18 -0600

    se añade encabezado a la aplicacion

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+
+# Encabezado de la aplicación
+st.header('Análisis de Datos de Vehículos Usados')
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
